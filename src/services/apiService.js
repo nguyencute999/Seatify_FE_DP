16b477bf594bdf6e33db98d5e2622b2e79e69d9c
nguyencute999 <nguyenlvce181235@fpt.edu.vn>
2025-11-01 19:11:38 +0700

    fix: url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://seatify-backend.azurewebsites.net/api/v1",
   withCredentials: true,
 });
 
